967b1356f53b401c8b67271baf5e3ed80c7b8412
Cory Walker <cory.walker.dev@gmail.com>
2023-02-01 07:37:00 -0800

    Fix month

--- javascript/update-clock.js
+++ javascript/update-clock.js
@@ -3,7 +3,7 @@ function updateClock() {
   months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   time = now.getHours() + ':' + ("0" + now.getMinutes()).slice(-2),
 
-  date = [months[now.getMonth() + 1],
+  date = [months[now.getMonth()],
           now.getDate(), 
           now.getFullYear()].join(' ');
 
